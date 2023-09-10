8f5a7f8de479d39cfbff78587e73e3411edee391
AdamsD02 <20co02@aitdgoa.edu.in>
2023-09-10 14:10:14 +0530

    Added file

---
+++ Task3/Start.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            padding-top: 50px;
+            padding-bottom: 50px;
+        }
+        div {
+            min-height: 100px;
+            min-width: 100px;
+            background-color: lightgreen;
+        }
+        div:hover {
+            background-color: green;
+        }
+    </style>
+</head>
+<body>
+    <div>Login</div>
+    <div>Sign Up</div>
+</body>
+</html>
