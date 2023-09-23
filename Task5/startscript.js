c50b8f9da3abaaca432b0532b8c05f3b148bd159
AdamsD02 <20co02@aitdgoa.edu.in>
2023-09-23 13:50:21 +0530

    Updated.

--- Task5/startscript.js
+++ Task5/startscript.js
@@ -1,51 +1,53 @@
 
-// API-key: AIzaSyCD9S7Nei4tqpky64zpMmPaQb5J-oTC64w
+const data = `{"doctor":[` +
+    `{"id":123,"name":"Dr.Nazareth","Lat":15.5895876,"Long":73.8166126}, `+
+    `{"id":456,"name":"Dr.Gomes","Lat":15.6313672,"Long":73.7784649}, `+
+    `{"id":789,"name":"Dr.Sankalp","Lat":15.4823288,"Long":73.8169355}, `+
+    `{"id":101,"name":"Dr.Jonas","Lat":15.044508,"Long":73.994300}, `+
+    `{"id":121,"name":"Dr.L.M.Sharma","Lat":15.381033,"Long":74.133553}] }`
+
 let x = document.getElementById("d2");
-let YOUR_KEY = "AIzaSyCD9S7Nei4tqpky64zpMmPaQb5J-oTC64w" ;
 
-function selectClicked(ch) {
-    const el = document.getElementById("selectbox") ;
-    switch(ch) {
-        case 1: el.style.boxShadow = 'none' ;
-          break ;
-        case 2: el.style.boxShadow = 'inset 0px 0px 5px gray' ;
-          displayList() ;
-          break ;
-        default: break;
+let leti = 15.5706;
+let longi = 73.8262; // 15.580899736403309,73.82628579807233
+let map;
+
+async function display() {
+  map = new google.maps.Map(document.getElementById("mapholder"), {
+    center: { lat: leti, lng: longi },
+    zoom: 9,
+  });
+}
+
+async function pointMarker(mId) {
+  let dName ="" ;
+  const va = JSON.parse(data) ;
+  for (i in va.doctor ) {
+    if( va.doctor[i]["id"] == mId ) {
+      leti = va.doctor[i]["Lat"] ;
+      longi = va.doctor[i]["Long"] ;
+      dName = va.doctor[i]["name"] ;
+      console.log(va.doctor[i] ) ;
+      break ;
     }
+  }
+  const mapOptions = {
+    zoom: 11,
+    center: { lat: leti, lng: longi }
+  };
+  // map = new google.maps.Map(document.getElementById("mapholder"), mapOptions) ;
+
+  const marker = new google.maps.Marker({
+    position: {lat: leti, lng: longi }, 
+    map: map
+  }) ;
+  const infowindow = new google.maps.InfoWindow({
+    content: "<p>Marker Location:" + marker.getPosition() + "<br>Name: " + dName + "</p>",
+  });
+  google.maps.event.addListener( marker, "mouseover", () => { infowindow.open(map, marker) ; } );
+  google.maps.event.addListener( marker, "mouseout", () => { infowindow.close() ;} ) ;
 }
 
-
-function closelist() {
-    let list = document.getElementById("details") ;
-    list.style.visibility = 'hidden' ;
-}
-
-function displayList() {
-  let list = document.getElementById("details") ;
-  list.style.visibility = 'visible' ;
-}
-
-//   x.innerHTML = "<br> Hi!" ;
-//   if (navigator.geolocation) {
-//     navigator.geolocation.watchPosition(showPosition);
-//   } else {
-//     x.innerHTML = "Geolocation is not supported by this browser.";
-//   }
-// }
-
-// function showPosition(position) {
-//   x.innerHTML += "Latitude: " + position.coords.latitude +
-//     "<br>Longitude: " + position.coords.longitude + 
-//     "Accuracy: " + position.coords.accuracy +
-//     "<br> Altitude: " + position.coords.altitude ;
-
-//     let lat = 30.02 ;
-//     let lon = 73.85 ;
-//     let latlon = lat + ',' + lon ;
-//     let img_url = "https://maps.googleapis.com/maps/api/staticmap?center=" + 
-//       latlon + "&zoom=14&size=400x300&sensor=false&key=" + YOUR_KEY ;
-//     document.getElementById("mapholder").innerHTML = 
-//       "<img src='" + img_url + "'> " ;
-
-// }
+// window.initMap = initMap ;
+window.display = display ;
+window.pointMarker = pointMarker ;
